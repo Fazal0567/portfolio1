56e142539827dfbdb933d6aef04e3a49e92ffc49
Fazal0567 <fazalalicontribute@gmail.com>
2025-07-09 05:08:46 +0530

    fix error

--- portfolio/src/components/AboutSection.jsx
+++ portfolio/src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
                 href="./public/Fazal_Ali_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300" download
               >
-                Download CV
+                Download Resume
               </a>
             </div>
           </div>
